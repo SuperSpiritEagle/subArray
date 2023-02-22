a8f4ff2ffa475371a0243c3c9319ec8d744d8898
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-02-22 14:28:42 +0500

    init

---
+++ SubarrayRepetitions/SubarrayRepetitions/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SubarrayRepetitions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elements = 30;
+            int[] numbers = new int[elements];
+            int minNumber = 0;
+            int maxNumber = 10;
+            int maxCount = 0;
+            int count = 1;
+            string copyNumber = "";
+
+            Random random = new Random();
+
+            for (int i = 0; i < elements; i++)
+            {
+                numbers[i] = random.Next(minNumber, maxNumber);
+            }
+
+            for (int i = 0; i < elements; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < elements - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    count++;
+
+                    if (maxCount < count)
+                    {
+                        maxCount = count;
+                        copyNumber = numbers[i].ToString();
+                    }
+                    else if (maxCount == count)
+                    {
+                        copyNumber += numbers[i].ToString();
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            
+            for (int i=0; i<copyNumber.Length;i++)
+            {
+                Console.WriteLine(copyNumber[i]+" Повторяется " + maxCount + " раз");
+            } 
+        }
+    }
+}
